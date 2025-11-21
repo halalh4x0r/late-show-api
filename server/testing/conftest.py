9354abb280ff7dbac79c5369f2087365914f6a41
halalh4x0r <mohamed.hadi@student.moringaschool.com>
2025-11-21 10:30:27 +0300

    modified conftest.py

--- server/testing/conftest.py
+++ server/testing/conftest.py
@@ -1,24 +1,18 @@
+import os
 import pytest
 from server.app import app, db
 
-@pytest.fixture(scope="session")
+@pytest.fixture
 def test_app():
-    app.config.update({
-        "TESTING": True,
-        "SQLALCHEMY_DATABASE_URI": "sqlite:///:memory:",
-        "SQLALCHEMY_TRACK_MODIFICATIONS": False
-    })
+    app.config["TESTING"] = True
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///:memory:"
 
     with app.app_context():
         db.create_all()
         yield app
+        db.session.remove()
         db.drop_all()
 
 @pytest.fixture
 def client(test_app):
     return test_app.test_client()
-
-@pytest.fixture
-def session(test_app):
-    with test_app.app_context():
-        yield db.session
